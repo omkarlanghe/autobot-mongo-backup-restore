38a983e61466a6ab1aebd4592a8d7dbe8089e113
omkarlanghe <langhe.omkar96@gmail.com>
2020-04-23 13:33:18 +0530

    configuration file updated

--- src/mongo-config.js
+++ src/mongo-config.js
@@ -3,28 +3,51 @@ const path = require('path');
 const dbBackupOptions = {
     host: 'localhost',
     port: 27017,
-    database: 'ems_db',
+    database: 'db-name-parent',
     autoBackup: true,
     removeOldBackup: true,
     keepLastDaysBackup: 1,
     logFileName: 'backup-logs.txt',
-    autoBackupPath: path.resolve('/EMS/Version-3/New_Repos/autobot-mongo-backup-restore/backup'),
-    logFilePath: path.resolve('/EMS/Version-3/New_Repos/autobot-mongo-backup-restore/backup')
+    autoBackupPath: path.resolve('your_path'),
+    logFilePath: path.resolve('your_path')
 };
 
 // database connection string
 const database = {
     url: 'mongodb://127.0.0.1:27017/',
-    name: ['ems_db_dev', 'ems_db_test']
+    name: ['db-name1', 'db-name2']
 }
 
 const dbRestoreOptions = {
     host: 'localhost',
     port: 27017,
     logFileName: 'restore-logs.txt',
-    sourcePath: path.resolve('/EMS/Version-3/New_Repos/autobot-mongo-backup-restore/backup'),
-    logFilePath: path.resolve('/EMS/Version-3/New_Repos/autobot-mongo-backup-restore/backup'),
+    sourcePath: path.resolve('your_path'),
+    logFilePath: path.resolve('your_path'),
     drop: true
 }
 
-module.exports = { dbBackupOptions, database, dbRestoreOptions };
+const collectionRestoreOptions = {
+    host: 'localhost',
+    port: 27017,
+    logFileName: 'collection-restore-logs.txt',
+    sourcePath: path.resolve('your_path'),
+    logFilePath: path.resolve('your_path'),
+    database: ['db-name'],
+    collections: [
+        'collection-name 1',
+        'collection-name 2',
+        'collection-name 3',
+        'collection-name 4',
+        'collection-name 5',
+        'abc'
+    ],
+    drop: true
+}
+
+module.exports = { 
+    dbBackupOptions, 
+    database, 
+    dbRestoreOptions, 
+    collectionRestoreOptions 
+};
